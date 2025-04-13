2bf1a2b456034d82452866144a52ab9e39b88f07
vikas <>
2025-04-13 23:55:20 +0530

    QuotesProjectJson

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/navbar"
+import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 import Quotes from "./components/Quotes"
 import './index.css'
